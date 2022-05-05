e327ab9e188704350730bc0740bfee2f81b62b38
Ryam Sousa <ryam.sousa123@gmail.com>
2022-05-05 20:55:41 -0300

    :sparkles: added question 35

---
+++ src/main/question35/Main.java
@@ -0,0 +1,27 @@
+package main.question35;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Schedule mySchedule = new Schedule(5);
+
+        mySchedule.setSchedule(1, 1, 2022, "Dentista", 0);
+        mySchedule.setSchedule(1, 5, 2022, "Mercado", 1);
+        mySchedule.setSchedule(5, 2, 2022, "Dentista", 2);
+        mySchedule.setSchedule(5, 2, 2022, "Dentista", 3);
+        mySchedule.setSchedule(2, 2, 2022, "Dentista", 4);
+
+        System.out.println("Digite o dia, mês e ano de uma das datas a seguir: ");
+        System.out.println(mySchedule);
+        System.out.println("Dia: ");
+        int day = scanner.nextInt();
+        System.out.println("Mês: ");
+        int month = scanner.nextInt();
+        System.out.println("Ano: ");
+        int year = scanner.nextInt();
+
+        System.out.println(mySchedule.listDay(day, month, year));
+    }
+}
--- src/main/question35/Schedule.java
+++ src/main/question35/Schedule.java
@@ -1,27 +1,32 @@
 package main.question35;
 
+import java.util.Arrays;
+
 public class Schedule {
+
     EntryInSchedule[] schedules;
 
-    Schedule(int length){
+    public Schedule(int length) {
         this.schedules = new EntryInSchedule[length];
     }
 
     public String listDay(int day, int month, int year){
         for(int i = 0; i < schedules.length; i++) {
            if( schedules[i].isInTheDay(day, month, year)){
-                return schedules[i].toString();
+               return schedules[i].toString();
             }
-
-            return "";
         }
-
-        return "";
+        return "Dia não encontrado";
     }
 
     public void setSchedule(int day, int month, int year, String subject, int index){
         schedules[index] = new EntryInSchedule(day, month, year, subject);
     }
 
-
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "schedules=" + Arrays.toString(schedules) +
+                '}';
+    }
 }
